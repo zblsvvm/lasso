4df9ed6cfd80c5d96893f49c171c40079833ef45
zblsvvm <svvm22@durham.ac.uk>
2020-01-09 17:57:01 +0000

    coordinate descent Modification

--- LinearRegression.py
+++ LinearRegression.py
@@ -67,29 +67,30 @@ class LinearRegression:
     def fit_cd(self, X, y, lamb=0.2, threshold=0.1):
         """ coordinate descent Method to get Lasso Regression Coefficient"""
         # Calculate RSS(residual sum of square)
-        rss = lambda X, y, w: (y - X * w).T * (y - X * w)
+        rss = lambda X, y, w: np.sum((y - X.dot(w)) ** 2)
+
         # initialize w.
         m, n = X.shape
-        w = np.matrix(np.zeros((n, 1)))
+        w = np.zeros((n, 1))
         r = rss(X, y, w)
         # CD method
         niter = itertools.count(1)
         for it in niter:
             for k in range(n):
                 # z_k and p_k calculation
-                z_k = (X[:, k].T * X[:, k])[0, 0]
+                z_k = np.dot(X[:, k].T , X[:, k])
                 p_k = 0
                 for i in range(m):
-                    p_k += X[i, k] * (y[i, 0] - sum([X[i, j] * w[j, 0] for j in range(n) if j != k]))
+                    p_k += X[i, k] * (y[i] - sum([X[i, j] * w[j] for j in range(n) if j != k]))
                 if p_k < -lamb / 2:
                     w_k = (p_k + lamb / 2) / z_k
                 elif p_k > lamb / 2:
                     w_k = (p_k - lamb / 2) / z_k
                 else:
                     w_k = 0
-                w[k, 0] = w_k
+                w[k] = w_k
             r_prime = rss(X, y, w)
-            delta = abs(r_prime - r)[0, 0]
+            delta = abs(r_prime - r)
             r = r_prime
             print('Iteration: {}, delta = {}'.format(it, delta))
             if delta < threshold:
@@ -157,4 +158,62 @@ class LinearRegression:
     def fit_mgd(self, X_train, y_train, lamb=0):
         theta = self.fit_sgd(X_train, y_train, lamb=lamb).theta
         self.theta = self.fit_bgd(X_train, y_train, initial_theta=theta, lamb=lamb).theta
+        return self
+    def fit_pgd(self, X, y):
+        def obj(X, w, y, lambd):
+            return f(X, w, y) + lambd * np.sum(np.abs(w))
+
+        def f(X, w, y):
+            Xw_b = X.dot(w) - y
+            return 0.5 * (Xw_b.T.dot(Xw_b))
+
+        def grf(X, w, y):
+            return X.T.dot(X.dot(w) - y)
+
+        # Model function
+        def m(w, wk, X, y, GammaK):
+            innerProd = grf(X, wk, y).T.dot(w - wk)
+            wDiff = w - wk
+            return f(X, wk, y) + innerProd + (1.0 / (2.0 * GammaK)) * wDiff.T.dot(wDiff)
+
+        # Shrinkage
+        def proxNorm1(v, lambd):
+            return np.sign(v) * np.maximum(np.zeros(np.shape(v)), np.abs(v) - lambd)
+
+        def acc(X, y, niter=500):
+            # initialize w.
+            m, n = X.shape
+            w = np.zeros((n, 1))
+            lambd = np.sqrt(2 * n * np.log(n)).tolist()
+            beta = 0.75
+            wk = w
+            yk = wk
+            t = 1
+            Diff = 0
+            for k in range(niter):
+                Gammak = 0.01
+
+                # Back Tracking Line Search
+                while True:
+                    wk_acc = yk - Gammak * grf(X, yk, y)
+                    if f(X, wk_acc, y) <= m(wk_acc, wk, X, y, Gammak):
+                        break
+                    else:
+                        Gammak = beta * Gammak
+                wk_acc = proxNorm1(wk, Gammak * lambd)
+                tk = 0.5 + 0.5 * np.sqrt(1 + 4 * (t ** 2.))
+                yk_acc = wk_acc + ((tk - 1) / (tk + 1)) * (wk_acc - wk)
+
+                Diff = np.linalg.norm(obj(X, wk_acc, y, lambd) - obj(X, wk, y, lambd))
+
+                wk = wk_acc
+                t = tk
+                yk = yk_acc
+
+                if Diff < 1:
+                    break
+
+            return wk
+
+        self.theta = acc(X, y)
         return self
