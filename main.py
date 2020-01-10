66d63d1ff4f8c375a2f39c067f96ab4840c8aa64
zblsvvm <svvm22@durham.ac.uk>
2020-01-10 03:07:42 +0000

    plt_square_lambs Modification

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 from LassoRegression import LassoRegression
-from data import Data
 import numpy as np
 from model_selection import train_test_split, cross_val_score
 from read_lasso_file import readfile
@@ -11,12 +10,17 @@ from visualisation import plt_residu_lambs
 from visualisation import plt_scores_datasize
 from visualisation import plt_square_lambs
 from visualisation import plt_coefs_coefs
+from sklearn.decomposition import PCA
+from model_selection import _k_split
 
 # Depending on your actual file location
 
 X, y = readfile('C:/Users/82569/Desktop/Residuals_Match_DMhydro_Less_z.txt')
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, seed=666)
+
+
 
 method = "bgd"
 
@@ -28,12 +32,21 @@ method = "bgd"
 
 #plt_scores_lambs(X_train, y_train, X_test, y_test, method)
 
-#plt_pred_obser(X_train, y_train, X_test, y_test, method, lamb=0)
+#plt_pred_obser(X_train, y_train, X_test, y_test, method, lamb=0.2)
 
 #plt_residu_lambs(X_train, y_train, X_test, y_test, method)
 
 #plt_scores_datasize(X_train, y_train, X_test, y_test, method)
 
-#plt_square_lambs(X_train, y_train, X_test, y_test, method)
+#plt_square_lambs(X, y, 5, method)
 
 #plt_coefs_coefs(X_train, y_train, method)
+
+
+
+
+
+
+
+
+
