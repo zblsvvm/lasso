f261b510648537269f81b7bdc99e055f2cba1836
zblsvvm <svvm22@durham.ac.uk>
2020-01-12 18:13:30 +0000

    old version

--- read_lasso_file.py
+++ read_lasso_file.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 # Depending on the actual file location
-# C:/Users/82569/Desktop/2222.txt
 def readfile(location):
     # number of features
     nof = tuple(np.arange(8))
@@ -17,7 +16,5 @@ def readfile(location):
     v = np.loadtxt(location, skiprows=1, usecols=exp)
     return m, v
 
-loc = 'C:/Users/82569/Desktop/Residuals_Match_DMhydro_Less_z.txt'
-m, c = readfile(loc)
-print(m)
-print(c)
+
+
